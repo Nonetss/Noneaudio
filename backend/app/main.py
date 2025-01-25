4b13594efd009a87186b616aa1b2351b4660467b
Nonetss <amorenolopezbarajas@gmail.com>
2025-01-25 18:52:52 +0100

    Modificamos el dockerfile backend

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,25 +1,8 @@
 # app/main.py
-from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
-
 from app.routes.audio import router as audio_router
+from fastapi import FastAPI
 
 app = FastAPI()
-
-origins = [
-    "http://localhost:3000",
-    "http://127.0.0.1:3000",
-    # otros orígenes si hacen falta
-]
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
-# Resto de configuración...
 
 
 def create_app() -> FastAPI:
